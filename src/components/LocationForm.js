a63415874d402998507847e82f7bd53592c107da
Jordan Bruce <jeb79@njit.edu>
2024-02-19 11:31:10 -0500

    initialized repository for react weather project

---
+++ src/components/LocationForm.js
@@ -0,0 +1,32 @@
+//imports go here
+
+import { useState } from "react";
+
+
+const LocationForm = ({setLocation}) => {
+	const [city, setCity] = useState("");
+
+	const handleLocationSearch = (event) => {
+		event.preventDefault();
+		setLocation({
+			city: city,
+		});
+		setCity("");		//clears the search box so the city value set to empty string "".
+	}
+
+	return (
+			<form className="location-form" onSubmit={handleLocationSearch}>
+					<label>
+						Location:
+						<input
+							type="text"
+							value={city}
+							onChange={(e) => setCity(e.target.value)}
+						/>
+					</label>
+					<input type="submit" value="Search"/>
+			</form>
+	);
+}
+
+export default LocationForm;
