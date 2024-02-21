7ad2840221af499781536169e6581e70f984f2e6
Jordan Bruce <jeb79@njit.edu>
2024-02-21 10:20:24 -0500

    added images for the weather thubmnail and created image component for current weather.

--- src/components/CurrentWeather/CurrentWeather.js
+++ src/components/CurrentWeather/CurrentWeather.js
@@ -5,6 +5,31 @@
 //imports go here
 import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
+import Image from "react-bootstrap/Image";
+
+//image imports
+import rainCloud from '../../images/rainCloud.png';
+import partlyCloudy from '../../images/partlyCloudy.png';
+import sun from '../../images/sun.png';
+
+
+/**
+ * The image thumbnail component for the current weather forecast component
+ */
+
+const weatherThumbnail = () => {
+	const currentWeatherMap = new Map([
+		['Sunny', sun],
+		['Partly cloudy', partlyCloudy],
+		['Rain', rainCloud],
+	]);
+
+	return <Image src="" alt="" />
+}
+
+/**
+ * The main current weather react component
+ */
 
 const CurrentWeather = ({
 	className,
@@ -14,7 +39,7 @@ const CurrentWeather = ({
 	const [currentWeather, setCurrentWeather] = useState(null);
 
 	useEffect(() => {
-		location.city && fetch('http://api.weatherstack.com/current?access_key=bfc1359347b2fac139c86edd5d68eb81&query=' + location.city)
+		location.city && fetch(`http://api.weatherstack.com/current?access_key=bfc1359347b2fac139c86edd5d68eb81&query=${location.city}`)
 			.then((response) => response.json())
 			.then((data) => {
 				setCurrentWeather(data);
@@ -28,7 +53,7 @@ const CurrentWeather = ({
 		<Card>
 			<Card.Body>
 				<Card.Text>
-					{currentWeather ? JSON.stringify(currentWeather) : null}
+					{currentWeather ? JSON.stringify(currentWeather.current) : null}
 				</Card.Text>
 			</Card.Body>
 		</Card>
