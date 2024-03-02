dadd645d7529d6dab96a4bb8e049fd783f3a1d43
Jordan Bruce <jeb79@njit.edu>
2024-03-02 09:51:48 -0500

    Fixed the bug that calls the weather api 1000 times. It was the Weather Forecast component that was causing the bug.

--- src/components/WeatherForecast/WeatherForecast.js
+++ src/components/WeatherForecast/WeatherForecast.js
@@ -1,17 +1,7 @@
 //imports go here
 import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
-import moment from "moment";
 
-function getCurrentDate() {
-	const dateObj = new Date();
-	return String(dateObj.toISOString().split('T')[0]);
-}
-
-function getFutureDate(numDaysFromNow) {
-  const futureDate = moment().add(numDaysFromNow, 'days').calendar().split('/');
-  return `${futureDate[2]}-${futureDate[0]}-${futureDate[1]}`; //formats the date as 'YYYY/MM/DD'
-}
 
 /**
  * The default Weather Forecast component.
@@ -19,7 +9,6 @@ function getFutureDate(numDaysFromNow) {
 
 const WeatherForecast = ({location}) => {
   const [forecast, setForecast] = useState({
-    dateRange: '7',
     weatherData: null
   });
   
@@ -32,18 +21,17 @@ const WeatherForecast = ({location}) => {
       ['Overcast', require('../../images/overcast.png')]
     ]);
 
-    location.city && fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=${location.city}&forecastDays=${String(forecast.dateRange)}&aggregateHours=24&unitGroup=us&shortColumnNames=false&contentType=json&key=${visualCrossingApiKey}`)
+    location.city && fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=${location.city}&forecastDays=10&aggregateHours=24&unitGroup=us&shortColumnNames=true&contentType=json&key=${visualCrossingApiKey}`)
       .then((response) => response.json())
       .then((data) => {
         setForecast({
-          ...forecast,
           weatherData: data,
         });
       })
       .catch((error) => {
         console.log(error);
       })
-  }, [location, forecast]);
+  }, [location]);
 
   return (
     <Card>
