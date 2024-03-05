286dd74ed431fd4b633bd0c538cdaf68e23398b2
Jordan Bruce <jeb79@njit.edu>
2024-03-05 10:19:50 -0500

    Implemented method of showcasing weather forecast for future 9 days.

--- src/components/WeatherForecast/WeatherForecast.js
+++ src/components/WeatherForecast/WeatherForecast.js
@@ -2,6 +2,46 @@
 import { useEffect, useState } from "react";
 import Card from "react-bootstrap/Card";
 
+/**
+ * The component for the single day weather description of the entire forecast
+ * 
+ * NOTE:
+ *  -the weatehrData prop key value is 'weatherData'. The value is the actual weatehrdata for that single day of the forecast.
+ */
+
+const SingleDayForecast = (weatherData) => {
+  const dateObj = new Date(weatherData.weatherData.datetime);
+  function getDate(dateObj) {
+    const monthMap = {
+      0: 'Jan',
+      1: 'Feb',
+      2: 'Mar',
+      3: 'Apr',
+      4: 'May',
+      5: 'Jun',
+      6: 'Jul',
+      7: 'Aug',
+      8: 'Sep',
+      9: 'Oct',
+      10: 'Nov',
+      11: 'Dec'
+    }
+    return (`${monthMap[dateObj.getMonth()]} ${dateObj.getDate()}`);
+  }
+  
+  return (
+    <Card>
+      <Card.Body>
+        <Card.Text>
+          {JSON.stringify(weatherData.weatherData.conditions)}
+          {JSON.stringify(weatherData.weatherData.temp)}
+          {JSON.stringify(getDate(dateObj))}
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
 
 /**
  * The default Weather Forecast component.
@@ -24,8 +64,10 @@ const WeatherForecast = ({location}) => {
     location.city && fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/weatherdata/forecast?locations=${location.city}&forecastDays=10&aggregateHours=24&unitGroup=us&shortColumnNames=true&contentType=json&key=${visualCrossingApiKey}`)
       .then((response) => response.json())
       .then((data) => {
+        const originalData = data.locations[location.city].values;  //'originalData' includes the current day as well. Current weather day is already displayed in another component.
+        const convertedData = originalData.slice(1, originalData.length);
         setForecast({
-          weatherData: data,
+          weatherData: convertedData,
         });
       })
       .catch((error) => {
@@ -37,6 +79,13 @@ const WeatherForecast = ({location}) => {
     <Card>
       <Card.Body>
         <Card.Title>Forecast</Card.Title>
+        {forecast.weatherData && forecast.weatherData.map((singleDayForecast) => {
+          return (
+            <SingleDayForecast
+              weatherData={singleDayForecast}
+            />
+          );
+        })}
         {forecast.weatherData ? JSON.stringify(forecast.weatherData) : null}
       </Card.Body>
     </Card>
